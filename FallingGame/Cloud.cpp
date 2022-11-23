3bd878a0005132e95255f873e35640141fdede5a
Kalle Hedberg <kallehed@gmail.com>
2022-11-23 10:48:02 +0100

    Added cloud methods

--- FallingGame/Cloud.cpp
+++ FallingGame/Cloud.cpp
@@ -1,1 +1,13 @@
 #include "Cloud.h"
+
+#include "Game.h"
+
+void Cloud::logic(Game& g)
+{
+	x += 0.001f * g.l.dt;
+}
+
+void Cloud::draw(Game& g)
+{
+	g.d.draw_image(g.c, g.d.cloud_texture, x, y, 0.1f, 0.1f, 0.f);
+}
--- FallingGame/Cloud.h
+++ FallingGame/Cloud.h
@@ -1,7 +1,13 @@
 #pragma once
 
+#include "misc.h"
+
 class Cloud
 {
+public:
+	void logic(Game& g);
+	void draw(Game& g);
+	// z between 0.f and 1.f
 	float x, y, z;
 };
 
